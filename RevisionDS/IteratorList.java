6fed6bca7f9688f93e653cb10dbe9788cfbddc59
Amine L'ch <aminelch95@gmail.com>
2019-04-24 17:11:34 +0100

    Ajout de fichier iterator

---
+++ RevisionDS/IteratorList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorList{
+
+    public static void main ( String[] args){
+        ArrayList<Integer> liste = new ArrayList<>(); 
+        //Ajout des elem au ArrayList avec la methode add 
+        for(int i =0;i<5;i++){
+            liste.add(i);
+        }
+
+        Iterator<Integer> it = liste.iterator();
+        // tanque l'iterator a un successeur 
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
+    
+}
